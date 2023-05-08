8af799e62da5772093be3ffa06606bace0a80226
Karuchiu <karuchiu8@gmail.com>
2023-05-08 11:03:20 +0300

    Successfully displayed list of airports

--- app/src/main/java/com/flightsearch/ui/FlightSearchScreens.kt
+++ app/src/main/java/com/flightsearch/ui/FlightSearchScreens.kt
@@ -14,9 +14,12 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.flightsearch.R
 import com.flightsearch.data.Airport
@@ -41,35 +44,49 @@ fun FlightSearchApp(
 
         }
     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            OutlinedTextField(
+                value = airportSearch,
+                onValueChange = {airportSearch = it },
+                label = {
+                    Text(
+                        text = stringResource(R.string.search_airport),
+                        style = MaterialTheme.typography.body1
+                    )
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Password,
+                    imeAction = ImeAction.Done
+                ),
+                keyboardActions = KeyboardActions(
+                    onDone = {focusManager.clearFocus()}
+                ),
+                modifier = modifier.fillMaxWidth()
+            )
+            FlightScheduleScreen(flightSchedules = fullSchedule)
+        }
+        
 
 
     }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp)
-    ) {
-        OutlinedTextField(
-            value = airportSearch,
-            onValueChange = {airportSearch = it },
-            label = {
-                Text(
-                    text = stringResource(R.string.search_airport),
-                    style = MaterialTheme.typography.body1
-                )
-            },
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Password,
-                imeAction = ImeAction.Done
-            ),
-            keyboardActions = KeyboardActions(
-                onDone = {focusManager.clearFocus()}
-            ),
-            modifier = modifier.fillMaxWidth()
-        )
-    }
+
+}
+
+@Composable
+fun FlightScheduleScreen(
+    flightSchedules: List<Airport>,
+    modifier: Modifier = Modifier
+) {
+    FlightScheduleDetails(
+        flightSchedules = flightSchedules,
+        modifier = modifier
+    )
 }
 
 @Composable
@@ -77,12 +94,39 @@ fun FlightScheduleDetails(
     flightSchedules: List<Airport>,
     modifier: Modifier = Modifier
 ) {
-    LazyColumn(modifier = modifier, contentPadding = PaddingValues(vertical = 8.dp) ){
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(vertical = 8.dp)
+    ){
         items(
             items = flightSchedules,
             key = {it.id}
-        ){
-
+        ){ schedule ->
+            Row(
+                modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 16.dp, horizontal = 16.dp),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = schedule.iataCode,
+                    style = MaterialTheme.typography.body1.copy(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight(300)
+                    ),
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.padding(start = 10.dp, end = 10.dp)
+                )
+                Text(
+                    text = schedule.name,
+                    style = MaterialTheme.typography.body1.copy(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight(300)
+                    ),
+                    textAlign = TextAlign.Center,
+                    modifier = modifier.padding(start = 10.dp, end = 10.dp)
+                )
+            }
         }
     }
 }
