ef88fec85369310a5e68472a13ec887167ee9096
Karuchiu <karuchiu8@gmail.com>
2023-06-04 23:50:20 +0300

    created a companion object and used booleanPreferencesKey function to define a key and pass it a name

--- app/src/main/java/com/flightsearch/FavoritePreferencesRepository.kt
+++ app/src/main/java/com/flightsearch/FavoritePreferencesRepository.kt
@@ -2,9 +2,12 @@ package com.flightsearch
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.booleanPreferencesKey
 
 class FavoritePreferencesRepository(
     private val dataStore: DataStore<Preferences>
 ) {
-
+    private companion object {
+        val IS_FAVORITE = booleanPreferencesKey("is_favorite")
+    }
 }
