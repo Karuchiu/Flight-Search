27c3cf1661b5a89675a21eb95ff5e2fd7159c547
Karuchiu <karuchiu8@gmail.com>
2023-06-15 15:40:06 +0300

    implemented FlightRepository

--- app/src/main/java/com/flightsearch/data/FlightRepository.kt
+++ app/src/main/java/com/flightsearch/data/FlightRepository.kt
@@ -1,4 +1,24 @@
 package com.flightsearch.data
 
+import com.flightsearch.models.Airport
+import com.flightsearch.models.Favorite
+import kotlinx.coroutines.flow.Flow
+
 interface FlightRepository {
+
+    fun getAllAirports(): Flow<List<Airport>>
+
+    fun getAirportsByInput(query: String): Flow<List<Airport>>
+
+    fun getAllCodesExcept(code: String): Flow<List<String>>
+
+    fun getAirportByCodeFlow(code: String): Flow<Airport>
+
+    fun getAllFavorites(): Flow<List<Favorite>>
+
+    fun getSingleFavorite(departureCode: String, destinationCode: String):Flow<Favorite>
+
+    suspend fun deleteFavoriteFlight(flight: Favorite)
+
+    suspend fun insertFavoriteFlight(flight: Favorite)
 }
--- app/src/main/java/com/flightsearch/data/OfflineFlightRepository.kt
+++ app/src/main/java/com/flightsearch/data/OfflineFlightRepository.kt
@@ -1,4 +1,39 @@
 package com.flightsearch.data
 
+import com.flightsearch.models.Airport
+import com.flightsearch.models.Favorite
+import kotlinx.coroutines.flow.Flow
+
 class OfflineFlightRepository(private val flightDao: FlightDao): FlightRepository {
+    override fun getAllAirports(): Flow<List<Airport>> {
+        return flightDao.getAllAirports()
+    }
+
+    override fun getAirportsByInput(query: String): Flow<List<Airport>> {
+        return flightDao.getAirportsByInput(query)
+    }
+
+    override fun getAllCodesExcept(code: String): Flow<List<String>> {
+        return flightDao.getAllCodesExcept(code)
+    }
+
+    override fun getAirportByCodeFlow(code: String): Flow<Airport> {
+        return flightDao.getAirportByCodeFlow(code)
+    }
+
+    override fun getAllFavorites(): Flow<List<Favorite>> {
+        return flightDao.getAllFavorites()
+    }
+
+    override fun getSingleFavorite(departureCode: String, destinationCode: String): Flow<Favorite> {
+        return getSingleFavorite(departureCode,destinationCode)
+    }
+
+    override suspend fun deleteFavoriteFlight(flight: Favorite) {
+        flightDao.deleteFavoriteFlight(flight)
+    }
+
+    override suspend fun insertFavoriteFlight(flight: Favorite) {
+        flightDao.insertFavoriteFlight(flight)
+    }
 }
