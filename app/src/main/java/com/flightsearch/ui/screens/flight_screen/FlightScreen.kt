69f9796d56d43fd0c6b1f5895f15d9664c161a25
Karuchiu <karuchiu8@gmail.com>
2023-06-26 23:01:47 +0300

    Added flight_screen package

---
+++ app/src/main/java/com/flightsearch/ui/screens/flight_screen/FlightScreen.kt
@@ -0,0 +1,39 @@
+package com.flightsearch.ui.screens.flight_screen
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.flightsearch.navigation.NavigationDestination
+
+object FlightScreenDestination: NavigationDestination{
+    override val route: String = "flight_screen"
+    const val codeArg = "code"
+    val routeWithArgs = "$route/{$codeArg}"
+}
+
+@Composable
+fun FlightScreen() {
+    val viewModel: FlightViewModel = viewModel(factory = FlightViewModel.Factory)
+    val uiState = viewModel.uiState.collectAsState()
+
+    val context = LocalContext.current
+
+    Column {
+        FlightResults(
+            departureAirport = uiState.value.departureAirport,
+            destinationList = uiState.value.destinationList,
+            favoriteList = uiState.value.favoriteList,
+            onFavoriteClick = { s1: String, s2: String ->
+                viewModel.addFavoriteFlight(s1, s2)
+                if (viewModel.flightAdded){
+                    Toast.makeText(context, "Added Favorite", Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(context, "Removed Favorite", Toast.LENGTH_SHORT).show()
+                }
+            }
+        )
+    }
+}
---
+++ app/src/main/java/com/flightsearch/ui/screens/flight_screen/FlightViewModel.kt
@@ -0,0 +1,117 @@
+package com.flightsearch.ui.screens.flight_screen
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.flightsearch.FlightSearchApplication
+import com.flightsearch.data.FlightRepository
+import com.flightsearch.models.Airport
+import com.flightsearch.models.Favorite
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+class FlightViewModel(
+    savedStateHandle: SavedStateHandle,
+    val flightRepository: FlightRepository
+): ViewModel() {
+    private val _uiState = MutableStateFlow(FlightUiState())
+    val uiState: StateFlow<FlightUiState> = _uiState
+
+    private val airportCode: String = savedStateHandle[FlightScreenDestination.codeArg] ?: ""
+
+    var flightAdded: Boolean by mutableStateOf(false)
+
+    private var favoriteFlights = mutableListOf<Favorite>()
+    private var allAirports = mutableListOf<Airport>()
+    private var destinationAirports = mutableListOf<Airport>()
+
+    init {
+        viewModelScope.launch {
+            processFlightList(airportCode)
+        }
+    }
+
+    private fun processFlightList(airportCode: String) {
+
+        viewModelScope.launch {
+            flightRepository.getAllFavorites().collect{
+                favoriteFlights.clear()
+                favoriteFlights.addAll(it)
+            }
+
+            flightRepository.getAllAirports().collect{
+                allAirports.clear()
+                allAirports.addAll(it)
+            }
+
+            flightRepository.getAllAirportsByCode(airportCode).collect{
+                destinationAirports.clear()
+                destinationAirports.addAll(it)
+            }
+
+            val departureAirport = allAirports.first {it.iataCode == airportCode}
+            _uiState.update{
+                it.copy(
+                    code = airportCode,
+                    favoriteList = favoriteFlights,
+                    destinationList = destinationAirports,
+                    departureAirport = departureAirport
+                )
+            }
+        }
+    }
+
+    fun addFavoriteFlight(departureCode: String, destinationCode: String){
+        viewModelScope.launch {
+            val favorite: Favorite = flightRepository.getSingleFavorite(departureCode, destinationCode)
+
+            if (favorite == null){
+                val tmp = Favorite(
+                    departureCode = departureCode,
+                    destinationCode = destinationCode
+                )
+                flightAdded = true
+                flightRepository.insertFavoriteFlight(tmp)
+            }else{
+                flightAdded = false
+                flightRepository.deleteFavoriteFlight(favorite)
+            }
+
+            // Cheating, I am forcing a Recomposition
+            // I should be using Flow but am not sure how to atm
+            flightRepository.getAllFavorites().collect{
+                favoriteFlights.clear()
+                favoriteFlights.addAll(it)
+            }
+            _uiState.update {
+                uiState.value.copy(
+                    favoriteList = favoriteFlights,
+                )
+            }
+        }
+    }
+
+    companion object{
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application =
+                    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as FlightSearchApplication)
+                val flightRepository = application.container.flightRepository
+                FlightViewModel(
+                    this.createSavedStateHandle(),
+                    flightRepository = flightRepository
+                )
+            }
+        }
+    }
+}
